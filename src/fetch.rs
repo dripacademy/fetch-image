e258c65ba999bf994aac54d7945b3ae57f656026
youngtrashbag <yuule.smeker@gmail.com>
2021-03-15 12:20:28 +0100

    feat: build custom client

--- src/fetch.rs
+++ src/fetch.rs
@@ -1,9 +1,21 @@
-use reqwest::blocking::get;
+use reqwest::blocking::{get, Client};
+use reqwest::{redirect::Policy, Proxy};
+use std::time::Duration;
 
+fn build_client() -> Client {
+    Client::builder()
+        .timeout(Duration::from_secs(10))
+        .redirect(Policy::none())
+        //.redirect(Policy::limited(1))
+        // default tor proxy
+        //.proxy(Proxy::http("socks5h://127.0.0.1:9050").expect("Tor Proxy not running"))
+        .build().unwrap()
+}
 /// returns json from account_name on instagram
 pub fn get_profile_info(account_name: String) -> String {
-    get(format!("https://instagram.com/{}?__a=1", account_name))
-    .unwrap()
-    .text()
-    .unwrap()
+    let client = build_client();
+
+    client.get(format!("https://instagram.com/{}?__a=1", account_name))
+        .send().unwrap()
+        .text().unwrap()
 }
