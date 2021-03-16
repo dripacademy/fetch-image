e6bc92207f70057e32a0d92a12168eb0d9464282
youngtrashbag <yuule.smeker@gmail.com>
2021-03-16 08:51:39 +0100

    feat: login works
    altough it is detected as malicious

--- wwwroot/index.js
+++ wwwroot/index.js
@@ -11,43 +11,81 @@ lines.forEach(l => {
     if (l.charAt(0) != "#") {
         const [key, value] = l.split("=");
         config[key] = value;
-        console.log(key + ", " + value);
     }
 });
 
 (async () => {
     const browser = await puppeteer.launch({
-        args: {
-            "--incognito',
-        },
         headless: false,
-    );
+    });
 
     const page = await browser.newPage();
-    await page.setViewpost({width: 1200, height: 720});
-    await page.goto("https://www.instagram.com/accounts/login");
+    await page.setViewport({width: 1200, height: 720});
 
-    Authenticate(page, config["USERNAME"], config["PASSWORD"]);
+    try {
+        await page.goto("https://www.instagram.com/accounts/login", {waitUntil: "networkidle2"});
+
+        const [button] = await page.$x("//button[contains(., 'Accept')]");
+        if (button) {
+            await button.click();
+        }
+
+    } catch(error) {
+        console.error(error)
+    }
+
+    try {
+        await page.type("[name='username']", config["USERNAME"], {delay: 207});
+        await page.type("[name='password']", config["PASSWORD"], {delay: 430});
+        await page.click("[type='submit']", {delay: 1374});
+        await page.waitForSelector("[placeholder='Search']", { state: "visible" });
+        console.log("logged in!");
+    } catch(error) {
+        console.error(error)
+    }
 
     // jesus.nr1 placeholder accountUrl
-    await ScrapeImages(page, "https://www.instagram.com/jesus.nr1");
-})
+    //let imgs = await ScrapeImages(page, "https://www.instagram.com/jesus.nr1", 20);
 
-async function Authenticate(page, username, password) {
-    await page.type("[name='username']", username, {delay: 100});
-    await page.type("[name='password']", password, {delay: 100});
-    await page.click("[type='submit']");
-    page.waitForSelector("[placeholder='Search']", { state: "visible" });
+    // no longer needed
+    await browser.disconnect();
+})();
 
-    /*await Promise.all([
-        // hopefully types enter
-        await page.keyboard.type("\n"),
-        page.waitForNavigation({waitUntil: "networkidle0"}),
-    ]);*/
-}
+/*async function ScrapeImages(page, accountUrl, maxItemCount) {
+    this.maxItemCount = maxItemCount
+    var page = this.page
+    let previousHeight
+    var media = new Set()
+    var index = `.`
 
-async function ScrapeImages(page, accountUrl) {
     await page.goto(accountUrl);
 
-}
+    while (maxItemCount == null || media.size < maxItemCount) {
+        try {
+            previousHeight = await page.evaluate(`document.body.scrollHeight`)
+            await page.evaluate(`window.scrollTo(0, document.body.scrollHeight)`)
+            await page.waitForFunction(`document.body.scrollHeight > ${previousHeight}`)
+            await page.waitFor(1000)
+            this.spinner.text = chalk.yellow(`Scrolling${index}`)
 
+            const nodes = await page.evaluate(() => {
+                const images = document.querySelectorAll(`a > div > div.KL4Bh > img`)
+                return [].map.call(images, img => img.src)
+            })
+
+            nodes.forEach(element => {
+                if (media.size < maxItemCount) {
+                    media.add(element)
+                }
+            })
+
+            index = index + `.`
+        }
+        catch (error) {
+            console.error(error)
+            break
+        }
+    }
+
+    return media
+}*/
