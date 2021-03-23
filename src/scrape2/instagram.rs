b8306fdde3e2dc6d12e2d9c83ddb2f992f14fec5
youngtrashbag <yuule.smeker@gmail.com>
2021-03-23 07:53:02 +0100

    use (async) thirtyfour and add tokio as dep

--- src/scrape2/instagram.rs
+++ src/scrape2/instagram.rs
@@ -1,39 +1,40 @@
-use thirtyfour_sync::prelude::*;
+use thirtyfour::prelude::*;
 
 pub struct Instagram {
     pub driver: WebDriver,
 }
 
 impl Instagram {
-    pub fn initialize() -> WebDriverResult<Instagram> {
+    pub async fn initialize() -> WebDriverResult<Instagram> {
         let mut caps = DesiredCapabilities::chrome();
         caps.add_chrome_arg("--lang=en")?;
+        caps.add_chrome_arg("--headlesss=false")?;
 
-        let driver = WebDriver::new("https://www.instagram.com/", &caps)?;
+        let driver = WebDriver::new("https://www.instagram.com/", &caps).await?;
 
         Ok(Instagram {
             driver: driver,
         })
     }
 
-    pub fn accept_cookies(&self) -> WebDriverResult<()> {
-        self.driver.find_element(By::XPath("//button[contains(., 'Accept')]"))?
-            .click()?;
+    pub async fn accept_cookies(&self) -> WebDriverResult<()> {
+        self.driver.find_element(By::XPath("//button[contains(., 'Accept')]")).await?
+            .click().await?;
 
         Ok(())
     }
 
-    pub fn log_in(&self, username: String, password: String) -> WebDriverResult<()> {
-        self.driver.get("https://www.instagram.com/accounts/login/")?;
+    pub async fn log_in(&self, username: String, password: String) -> WebDriverResult<()> {
+        self.driver.get("https://www.instagram.com/accounts/login/").await?;
 
-        self.driver.find_element(By::Name("username"))?
-            .send_keys(username)?;
+        self.driver.find_element(By::Name("username")).await?
+            .send_keys(username).await?;
 
-        self.driver.find_element(By::Name("password"))?
-            .send_keys(password)?;
+        self.driver.find_element(By::Name("password")).await?
+            .send_keys(password).await?;
 
-        self.driver.find_element(By::XPath("//button[contains(., 'Log In')]"))?
-            .click()?;
+        self.driver.find_element(By::XPath("//button[contains(., 'Log In')]")).await?
+            .click().await?;
 
         Ok(())
     }
--- src/scrape2/main.rs
+++ src/scrape2/main.rs
@@ -1,21 +1,28 @@
+extern crate tokio;
 #[macro_use]
 extern crate log;
 use env_logger::{Builder, Target};
 use std::env;
 use dotenv::dotenv;
 
-use thirtyfour_sync::prelude::*;
-
 mod instagram;
 use instagram::*;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     dotenv().ok();
 
     let mut log_builder = Builder::from_default_env();
     log_builder.target(Target::Stdout);
     log_builder.init();
 
-    let ig = Instagram::initialize().unwrap();
-    ig.accept_cookies().unwrap();
+    let ig: Option<Instagram> = match Instagram::initialize().await {
+        Err(e) => {
+            warn!("{}", e);
+            None
+        },
+        Ok(i) => Some(i),
+    };
+
+    ig.unwrap().accept_cookies().await.unwrap();
 }
